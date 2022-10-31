c71a5104411ecab886d1a0c71150c968f46d89a8
infoboyz <jorge12sam@gmail.com>
2022-10-31 19:12:35 -0400

    Criando as funções que armazenam todos os sons das teclas

--- main.js
+++ main.js
@@ -1,21 +1,48 @@
 //Trazendo do html todas as teclas de som
-document.querySelector('.tecla_pom');
-document.querySelector('.tecla_clap');
-document.querySelector('.tecla_tim');
-document.querySelector('.tecla_puff');
-document.querySelector('.tecla_splash');
-document.querySelector('.tecla_toim');
-document.querySelector('.tecla_psh');
-document.querySelector('.tecla_tic');
-document.querySelector('.tecla_tom');
+const pom = document.querySelector('.tecla_pom');
+const clap = document.querySelector('.tecla_clap');
+const tim = document.querySelector('.tecla_tim');
+const puff = document.querySelector('.tecla_puff');
+const spash = document.querySelector('.tecla_splash');
+const toim = document.querySelector('.tecla_toim');
+const psh = document.querySelector('.tecla_psh');
+const tic = document.querySelector('.tecla_tic');
+const tom = document.querySelector('.tecla_tom');
 
-//Trazendo do html todos os sons das teclas
-document.querySelector('#som_tecla_pom').play();
-document.querySelector('#som_tecla_clap').play();
-document.querySelector('#som_tecla_tim').play();
-document.querySelector('#som_tecla_puff').play();
-document.querySelector('#som_tecla_splash').play();
-document.querySelector('#som_tecla_toim').play();
-document.querySelector('#som_tecla_psh').play();
-document.querySelector('#som_tecla_tic').play();
-document.querySelector('#som_tecla_tom').play();
+//Criando as funções que armazenam todos os sons das teclas
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+function tocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+function tocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+function tocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+function tocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+function tocaSomTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+function tocaSomTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
+
