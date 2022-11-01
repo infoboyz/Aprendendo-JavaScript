13e4320d96dba7c010c46cb39e85a7514b562561
infoboyz <jorge12sam@gmail.com>
2022-11-01 11:38:58 -0400

    Percorrendo uma lista

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-function tocaSomPom(){
+//Declaracao de referencias
+const listadeTeclas = document.querySelectorAll('.tecla');
+let x = 0;
+
+//Funcoes
+function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-const listadeTeclas = document.querySelectorAll('.tecla');
-listadeTeclas[0].onclick = tocaSomPom
+//Estrutura de repeticao para por o som nas teclas
+while (x < listadeTeclas.length) {
+    listadeTeclas[x].onclick = tocaSomPom
+    x = x + 1;
+    console.log(x);
+}
