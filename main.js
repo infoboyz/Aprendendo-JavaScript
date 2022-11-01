2f871e6171e0a0c7c4bad0b67578629dc4ff17df
infoboyz <jorge12sam@gmail.com>
2022-11-01 15:51:35 -0400

    Realizando o terceiro desafio alura

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
+//Desafio 1
+document.querySelector('input[type=tel]')
+
+//Desafio 2
 const listadeTeclas = document.querySelectorAll('input[type=button]')
 console.log(listadeTeclas);
+
+//Desafio 3 
+const campo = document.querySelector('input[type=tel]')
+
+for (x = 0; x < listadeTeclas.length; x++){
+    const tecla = listadeTeclas[x];
+
+    tecla.onclick = function () {
+        campo.value += tecla.value;
+    }
+}
