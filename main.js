04c86539b37b29ddb4f61942d12892a554bd0971
infoboyz <jorge12sam@gmail.com>
2022-11-02 18:42:01 -0400

    Melhorando o codigo dnv

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const listadeTeclas = document.querySelectorAll('.tecla');
 //Funcoes
 function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
-    if (elemento != null && elemento.localName === 'audio') {
+    if (elemento && elemento.localName === 'audio') {
         elemento.play();
     }else {
         console.log('Elemento não encontrado ou seletor inválido');
